cb174cd0661620fa1e096d990ad7b824eaf0f851
Mihaly Hunyady <megamegax@gmail.com>
2016-11-01 17:43:34 +0100

    examples added for using the Retrofit

--- src/main/java/com/banktech/javachallenge/Main.java
+++ src/main/java/com/banktech/javachallenge/Main.java
@@ -1,6 +1,12 @@
 package com.banktech.javachallenge;
 
 import com.banktech.javachallenge.service.Api;
+import com.banktech.javachallenge.service.domain.game.GameResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.io.IOException;
 
 /**
  *
@@ -8,6 +14,33 @@ import com.banktech.javachallenge.service.Api;
 public class Main {
 
     public static void main(String[] args) {
+        startUp(args);
+        exampleApiCall();
+        exampleApiCallAsync();
+    }
+
+    private static void exampleApiCall() {
+        try {
+            Response<GameResponse> response = Api.gameService().listGames().execute();
+            GameResponse runningGames = response.body();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void exampleApiCallAsync() {
+        Api.gameService().listGames().enqueue(new Callback<GameResponse>() {
+            public void onResponse(Call<GameResponse> call, Response<GameResponse> response) {
+                GameResponse runningGames = response.body();
+            }
+
+            public void onFailure(Call<GameResponse> call, Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+    }
+
+    private static void startUp(String[] args) {
         if (args.length == 2) {
             setUpApi(args[0], args[1]);
         } else {
