609bb2e9653321216ec35b1129d9dc5ba8683041
Mihaly Hunyady <mihaly_hunyady@epam.com>
2016-11-04 16:01:09 +0100

    submarines added to viewModel

--- src/main/java/com/banktech/javachallenge/view/ViewModel.kt
+++ src/main/java/com/banktech/javachallenge/view/ViewModel.kt
@@ -1,5 +1,7 @@
 package com.banktech.javachallenge.view
 
 import com.banktech.javachallenge.service.domain.game.Game
+import com.banktech.javachallenge.service.domain.submarine.Submarine
+import com.banktech.javachallenge.service.domain.submarine.SubmarineResponse
 
-data class ViewModel(var game: Game, var calls: List<ApiCall>)
+data class ViewModel(var game: Game, var calls: List<ApiCall>, var ownSubmarines: List<SubmarineResponse>, var detectedSubmarines: List<Submarine>)
