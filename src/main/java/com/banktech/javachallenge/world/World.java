573119f97a2b967614fbb5927f8b989d485c8147
mihalyhunyady <megamegax@gmail.com>
2016-11-05 12:14:36 +0100

    add more javadoc comment

--- src/main/java/com/banktech/javachallenge/world/World.java
+++ src/main/java/com/banktech/javachallenge/world/World.java
@@ -6,9 +6,27 @@ import com.banktech.javachallenge.service.domain.submarine.Submarine;
 
 
 public interface World {
+
+    /**
+     * Gives back the Cell on the given {@link Position}.
+     *
+     * @param position {@link Position}
+     * @return {@link Submarine} or {@link Island} or {@link Torpedo} or Null if nothing is there.
+     */
     Object cellAt(final Position position);
 
+    /**
+     * Moves selected {@link Submarine} on the Map.
+     *
+     * @param submarine   {@link Submarine}
+     * @param moveRequest {@link MoveRequest}
+     */
     void move(final Submarine submarine, final MoveRequest moveRequest);
 
+    /**
+     * Gives back the width and height of the world.
+     *
+     * @return {@link Position} containing the width and height of the World
+     */
     Position size();
 }
